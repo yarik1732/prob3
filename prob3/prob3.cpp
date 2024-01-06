02cf5f7e82cc704e0371d784faeb64c5190ec45b
Yarik123121 <wertehuid@gmail.com>
2024-01-06 14:37:50 +0300

    Добавьте файлы проекта.

---
+++ prob3/prob3.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int name, ran, g = 2;
+	bool is_hol = false;
+	std::cout << "В ведите 1 число: ";
+	std::cin >> name;
+	std::cout << "В ведите 2 число: ";
+	std::cin >> ran;
+	if (is_hol == !true) g = 10;
+	if (name > ran) std::cout << "name" << g;
+	else if (name < ran) std::cout << "ran" << g;
+	else if (name == ran) std::cout << "0" << g;
+	
+}
+
